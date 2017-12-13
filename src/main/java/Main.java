a58d9fcd15cd70c0438a60752439ae2f6e01a4a1
Nhan Cao <nhancv92@gmail.com>
2017-12-13 14:45:14 +0700

    searching 1:1 matching

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,33 @@
+import com.machinezoo.sourceafis.FingerprintMatcher;
 import com.machinezoo.sourceafis.FingerprintTemplate;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+//https://sourceafis.machinezoo.com/
+
 public class Main {
 
-    public static void main() throws Exception {
-        byte[] probeImage = Files.readAllBytes(Paths.get("probe.jpeg"));
-        byte[] candidateImage = Files.readAllBytes(Paths.get("candidate.jpeg"));
+    public static void main(String[] args) throws Exception {
+
+        double threshold = 40; //FMR 0.01%
+
+        byte[] probeImage = Files.readAllBytes(Paths.get("db/101_1.jpg"));
+        byte[] candidateImage = Files.readAllBytes(Paths.get("db/101_2.jpg"));
+        byte[] candidateImage2 = Files.readAllBytes(Paths.get("db/101_3.jpg"));
         FingerprintTemplate probe = new FingerprintTemplate(probeImage);
         FingerprintTemplate candidate = new FingerprintTemplate(candidateImage);
+        FingerprintTemplate candidate2 = new FingerprintTemplate(candidateImage2);
+
+        System.out.println("-----START-----");
+
+        FingerprintMatcher matcher = new FingerprintMatcher(probe);
+        double score = matcher.match(candidate);
+        double score2 = matcher.match(candidate2);
+        System.out.println(String.format("Match with score: %f => Results: %s", score, score >= threshold));
+        System.out.println(String.format("Match with score: %f => Results: %s", score2, score2 >= threshold));
 
 
-
-
-
+        System.out.println("-----END-----");
     }
 }
